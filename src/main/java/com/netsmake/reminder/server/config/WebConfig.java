f56601599af4d9e23106f4f748c793f51213ddff
kseyawingit <callmeloya@mail.ru>
2017-01-26 15:12:00 +0300

    tomcat + controller + config

---
+++ src/main/java/com/netsmake/reminder/server/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.netsmake.reminder.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.netsmake.reminder.server")
+public class WebConfig extends WebMvcConfigurerAdapter{
+}
