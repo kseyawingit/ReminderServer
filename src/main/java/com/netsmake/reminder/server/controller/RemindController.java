f73bca7b7ce75f8678b1521d9cd7e298606f83aa
kseyawingit <callmeloya@mail.ru>
2017-01-27 19:59:01 +0300

    +JSON

--- src/main/java/com/netsmake/reminder/server/controller/RemindController.java
+++ src/main/java/com/netsmake/reminder/server/controller/RemindController.java
@@ -1,19 +1,41 @@
 package com.netsmake.reminder.server.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import com.netsmake.reminder.server.entity.Remind;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+//@Controller
+@RestController
 @RequestMapping("/remind")
 public class RemindController {
 
         @RequestMapping(value = "/get", method = RequestMethod.GET)
         @ResponseBody
+        /*
         public String getRemider(ModelMap model){
                 return "my reminder";
         }
+        */
+        /*
+        public Remind getRemider(ModelMap model) {
+            return createMockRemind();
+        }
+        */
+    public Remind getRemider() {
+            return createMockRemind();
+        }
+
+    private Remind createMockRemind() {
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setRemindDate(new Date());
+        remind.setTitle("My first remind");
+
+        return remind;
+    }
 
 }
